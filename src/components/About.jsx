19c3cd559a325a0839f82a606f501b237315da05
Luis Aleman <lalem014@fiu.edu>
2023-04-30 22:14:11 -0400

    Change wording

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,14 +15,13 @@ const About = () => {
 
         <p className="about-me mt-15">
           Hi there! I'm Luis, a front-end developer with a huge passion for
-          tech. In fact, my first word was actually "computer", I'm not kidding
-          (I am). In my free time, you can usually find me working out, playing
-          video games, bothering my cats, or language learning. I enjoy learning
-          new things as I feel it not only helps me grow as a person, but also
-          fuels my creativity and problem-solving skills as a developer. I am
-          planning to graduate with a Bachelor of Science in Software
-          Engineering in 2024, and I am excited to continue learning and growing
-          in my career.
+          tech. In my free time, you can usually find me working out, playing
+          video games, bothering my cats, or language learning. I spend a lot of
+          time learning new things, as I feel it not only helps me grow as a
+          person, but also fuels my creativity and problem-solving skills as a
+          developer. I am planning to graduate with a Bachelor of Science in
+          Software Engineering in 2024, and I am excited to continue learning
+          and growing in my career.
         </p>
       </div>
     </div>
